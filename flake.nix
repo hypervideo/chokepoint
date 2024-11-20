de6ee744a057726a0ac27ef515ca0c4aa817dd4f
Robert Krahn <robert.krahn@gmail.com>
2024-11-20 02:33:33 +0100

    rustfmt nightly

--- flake.nix
+++ flake.nix
@@ -6,33 +6,34 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
       in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              rustc
-              cargo
-              clippy
-              clang
-              pkg-config
-            ];
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            clippy
+            clang
+            pkg-config
+          ];
 
-            buildInputs = with pkgs; [
-              openssl
-            ];
+          buildInputs = with pkgs; [
+            openssl
+          ];
 
-            packages = with pkgs; [
-              rust-analyzer
-              rustfmt
-              cargo-nextest
-              cargo-readme
-            ];
+          packages = with pkgs; [
+            rust-analyzer
+            (rustfmt.override { asNightly = true; })
+            cargo-nextest
+            cargo-readme
+          ];
 
-            RUST_BACKTRACE = "1";
-            RUST_LOG = "debug";
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-          };
-        }
+          RUST_BACKTRACE = "1";
+          RUST_LOG = "debug";
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        };
+      }
     );
 }
