5557ca4b3120c8c9ea1d28a22901b5f2b28c6d4b
Robert Krahn <robert.krahn@gmail.com>
2024-11-20 19:42:19 +0100

    fix readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # chokepoint
 
-A generic stream transformer that can be used to "shape traffic", e.g. to simulate network conditions.
-
 [![Crates.io](https://img.shields.io/crates/v/chokepoint)](https://crates.io/crates/chokepoint)
 [![](https://docs.rs/chokepoint/badge.svg)](https://docs.rs/chokepoint)
 [![License](https://img.shields.io/crates/l/chokepoint?color=informational&logo=mpl-2)](/LICENSE)
+
+A generic stream transformer that can be used to "shape traffic", e.g. to simulate network conditions.
 
 Supports various simulated network conditions, such as:
 - Delay (using a user provided function)
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-//! A generic stream transformer that can be used to "shape traffic", e.g. to simulate network conditions.
-//!
 //! [![Crates.io](https://img.shields.io/crates/v/chokepoint)](https://crates.io/crates/chokepoint)
 //! [![](https://docs.rs/chokepoint/badge.svg)](https://docs.rs/chokepoint)
 //! [![License](https://img.shields.io/crates/l/chokepoint?color=informational&logo=mpl-2)](/LICENSE)
+//!
+//! A generic stream transformer that can be used to "shape traffic", e.g. to simulate network conditions.
 //!
 //! Supports various simulated network conditions, such as:
 //! - Delay (using a user provided function)
